af556e3745e7f564fcadcf5564a7e55b6f799602
minjyun3011 <grand3canyon10@gmail.com>
2023-08-28 12:17:58 +0900

    Initial Commit

---
+++ practicesyoryu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practicesyoryu
+//
+//  Created by 佐藤壮 on 2023/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
