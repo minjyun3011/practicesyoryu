3e230f83e2cbfaeab8629a1ca4fc751747d955b3
minjyun3011 <grand3canyon10@gmail.com>
2023-08-29 16:07:10 +0900

    昇龍拳できました！

--- practicesyoryu/ViewController.swift
+++ practicesyoryu/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let count = 20
+    var attackArray: [String] = []
+    
+    
+    @IBOutlet weak var animation: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        for index in 1 ... count {
+            
+            attackArray.append("attack" + String(index))
+            
+        }
+        print(attackArray)
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func syoryuken(_ sender: UIButton) {
+        for i in 0..<attackArray.count {
+            
+            
+            let delay = Double(i) * 0.1
+        
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                self.displayImage(withName: self.attackArray[i])
+                
+            }
+        }
+        displayImage(withName: attackArray[0])
+        
+    }
+    
+    func displayImage(withName attackName: String) {
+        print(attackName)
+        let image = UIImage(named: attackName)
+        animation.image = image
+    }
 }
-
